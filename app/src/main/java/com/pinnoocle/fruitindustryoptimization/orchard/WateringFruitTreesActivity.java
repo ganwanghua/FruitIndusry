8ae30f73b9b7011d6e936f8e8615376e70310cce
ganwanghua <1345895582@qq.com>
2021-04-07 09:53:00 +0800

    ws

--- app/src/main/java/com/pinnoocle/fruitindustryoptimization/orchard/WateringFruitTreesActivity.java
+++ app/src/main/java/com/pinnoocle/fruitindustryoptimization/orchard/WateringFruitTreesActivity.java
@@ -97,6 +97,16 @@ public class WateringFruitTreesActivity extends BaseActivity {
     LinearLayout llTreeInfo;
     @BindView(R.id.rl_collar_tree)
     RelativeLayout rlCollarTree;
+    @BindView(R.id.rl_fertilizer)
+    RelativeLayout rlFertilizer;
+    @BindView(R.id.iv_fertilizer)
+    ImageView ivFertilizer;
+    @BindView(R.id.iv_fertilizer1)
+    ImageView ivFertilizer1;
+    @BindView(R.id.iv_fertilizer2)
+    ImageView ivFertilizer2;
+    @BindView(R.id.iv_fertilizer3)
+    ImageView ivFertilizer3;
     private DataRepository dataRepository;
     private UserTreeDetailModel userTreeDetailModel;
 
@@ -132,7 +142,7 @@ public class WateringFruitTreesActivity extends BaseActivity {
                 if (userTreeDetailModel.getCode() == 1) {
                     Glide.with(WateringFruitTreesActivity.this).load(userTreeDetailModel.getData().getUser().getAvatarUrl()).apply(bitmapTransform(new GlideCircleTransform(WateringFruitTreesActivity.this))).into(ivAvatar);
                     tvName1.setText(userTreeDetailModel.getData().getUser().getNickName());
-                    tvName.setText(userTreeDetailModel.getData().getUser_tree().getName());
+
                     tvDay.setText(userTreeDetailModel.getData().getUser_tree().getLeft_day() + "");
                     tvTime.setText(userTreeDetailModel.getData().getUser_tree().getEnd_date() + "到期");
                     if (userTreeDetailModel.getData().getUser_coupons().size() > 0) {
@@ -140,14 +150,51 @@ public class WateringFruitTreesActivity extends BaseActivity {
                     } else {
                         tvNotice.setVisibility(View.GONE);
                     }
+                    if (TextUtils.isEmpty(userTreeDetailModel.getData().getUser_tree().getName())) {
+                        tvName.setText("挂个名字吧");
+                    } else {
+                        tvName.setText(userTreeDetailModel.getData().getUser_tree().getName());
+                    }
                 }
             }
         });
     }
 
-    @OnClick({R.id.iv_back, R.id.iv_wish, R.id.rl_big_gift_bag, R.id.ll_watering, R.id.rl_tree, R.id.ll_tree_info, R.id.rl_collar_tree})
+    @OnClick({R.id.tv_name, R.id.iv_back, R.id.iv_wish, R.id.rl_big_gift_bag, R.id.ll_watering, R.id.rl_tree, R.id.ll_tree_info, R.id.rl_collar_tree, R.id.rl_fertilizer})
     public void onViewClicked(View view) {
         switch (view.getId()) {
+            case R.id.tv_name:
+                new TDialog.Builder(getSupportFragmentManager())
+                        .setLayoutRes(R.layout.dialog_name)
+                        .setScreenWidthAspect(this, 1f)
+                        .setGravity(Gravity.BOTTOM)
+                        .setCancelableOutside(true)
+                        .addOnClickListener(R.id.tv_save, R.id.tv_cancel)
+                        .setOnBindViewListener(new OnBindViewListener() {
+                            @Override
+                            public void bindView(BindViewHolder viewHolder) {
+
+                            }
+                        })
+                        .setOnViewClickListener(new OnViewClickListener() {
+                            @Override
+                            public void onViewClick(BindViewHolder viewHolder, View view, TDialog tDialog) {
+                                switch (view.getId()) {
+                                    case R.id.tv_cancel:
+                                        tDialog.dismiss();
+                                        break;
+                                    case R.id.tv_save:
+                                        tDialog.dismiss();
+                                        break;
+                                }
+                            }
+                        })
+                        .create()
+                        .show();
+                break;
+            case R.id.rl_fertilizer:
+                grow("2");
+                break;
             case R.id.rl_collar_tree:
                 ActivityUtils.startActivity(this, AdoptActivity.class);
                 break;
@@ -226,7 +273,7 @@ public class WateringFruitTreesActivity extends BaseActivity {
                 startActivity(intent);
                 break;
             case R.id.ll_watering:
-                grow();
+                grow("1");
                 break;
             case R.id.rl_tree:
                 ScaleAnimation sa = new ScaleAnimation(1f, 1.0f, 1.0f, 1.1f, ScaleAnimation.RELATIVE_TO_SELF, 0.0f, ScaleAnimation.RELATIVE_TO_SELF, 1.0f);
@@ -259,13 +306,13 @@ public class WateringFruitTreesActivity extends BaseActivity {
         }
     }
 
-    private void grow() {
+    private void grow(String type) {
         ViewLoading.show(this);
         Map<String, String> map = new HashMap<>();
         map.put("s", "/api/tree/grow");
         map.put("wxapp_id", "10001");
         map.put("token", FastData.getToken());
-        map.put("type", "1");
+        map.put("type", type);
         map.put("tree_id", getIntent().getStringExtra("tree_id"));
         dataRepository.grow(map, new RemotDataSource.getCallback() {
             @Override
@@ -279,60 +326,117 @@ public class WateringFruitTreesActivity extends BaseActivity {
                 StatusModel statusModel = (StatusModel) data;
                 if (statusModel.getCode() == 1) {
 //                    GrowModel loginModel = new Gson().fromJson(statusModel.getData(), GrowModel.class);
-                    Animation operatingAnim = AnimationUtils.loadAnimation(WateringFruitTreesActivity.this, R.anim.rotate_anim);
-                    LinearInterpolator lin = new LinearInterpolator();
-                    operatingAnim.setInterpolator(lin);
-                    operatingAnim.setFillAfter(true);
-                    operatingAnim.setAnimationListener(new Animation.AnimationListener() {
-                        @Override
-                        public void onAnimationStart(Animation animation) {
-                            Animation animation1 = new AlphaAnimation(0f, 1.0f);
-                            animation1.setDuration(800);//动画的持续的时间
-                            animation1.setRepeatCount(1);
-                            ivWaterDrop.startAnimation(animation1);
+                    if (type.equals("1")) {
+                        Animation operatingAnim = AnimationUtils.loadAnimation(WateringFruitTreesActivity.this, R.anim.rotate_anim);
+                        LinearInterpolator lin = new LinearInterpolator();
+                        operatingAnim.setInterpolator(lin);
+                        operatingAnim.setFillAfter(true);
+                        operatingAnim.setAnimationListener(new Animation.AnimationListener() {
+                            @Override
+                            public void onAnimationStart(Animation animation) {
+                                Animation animation1 = new AlphaAnimation(0f, 1.0f);
+                                animation1.setDuration(800);//动画的持续的时间
+                                animation1.setRepeatCount(1);
+                                ivWaterDrop.startAnimation(animation1);
 
-                            Animation animation2 = new AlphaAnimation(0f, 1.0f);
-                            animation2.setDuration(800);//动画的持续的时间
-                            animation2.setRepeatCount(1);
-                            ivWaterDrop1.startAnimation(animation2);
+                                Animation animation2 = new AlphaAnimation(0f, 1.0f);
+                                animation2.setDuration(800);//动画的持续的时间
+                                animation2.setRepeatCount(1);
+                                ivWaterDrop1.startAnimation(animation2);
 
-                            Animation animation3 = new AlphaAnimation(0f, 1.0f);
-                            animation3.setDuration(800);//动画的持续的时间
-                            animation3.setRepeatCount(1);
-                            ivWaterDrop2.startAnimation(animation3);
-                        }
+                                Animation animation3 = new AlphaAnimation(0f, 1.0f);
+                                animation3.setDuration(800);//动画的持续的时间
+                                animation3.setRepeatCount(1);
+                                ivWaterDrop2.startAnimation(animation3);
+                            }
 
-                        @Override
-                        public void onAnimationEnd(Animation animation) {
-                            Animation operatingAnim = AnimationUtils.loadAnimation(WateringFruitTreesActivity.this, R.anim.rotate_anim_one);
-                            LinearInterpolator lin = new LinearInterpolator();
-                            operatingAnim.setInterpolator(lin);
-                            operatingAnim.setFillAfter(true);
-                            operatingAnim.setAnimationListener(new Animation.AnimationListener() {
-                                @Override
-                                public void onAnimationStart(Animation animation) {
-                                }
+                            @Override
+                            public void onAnimationEnd(Animation animation) {
+                                Animation operatingAnim = AnimationUtils.loadAnimation(WateringFruitTreesActivity.this, R.anim.rotate_anim_one);
+                                LinearInterpolator lin = new LinearInterpolator();
+                                operatingAnim.setInterpolator(lin);
+                                operatingAnim.setFillAfter(true);
+                                operatingAnim.setAnimationListener(new Animation.AnimationListener() {
+                                    @Override
+                                    public void onAnimationStart(Animation animation) {
+                                    }
 
-                                @Override
-                                public void onAnimationEnd(Animation animation) {
-                                    Animation animation1 = new AlphaAnimation(1.0f, 0f);
-                                    animation1.setDuration(1000);//动画的持续的时间
-                                    animation1.setFillAfter(true);
-                                    ivKettle.startAnimation(animation1);
-                                }
+                                    @Override
+                                    public void onAnimationEnd(Animation animation) {
+                                        Animation animation1 = new AlphaAnimation(1.0f, 0f);
+                                        animation1.setDuration(1000);//动画的持续的时间
+                                        animation1.setFillAfter(true);
+                                        ivKettle.startAnimation(animation1);
+                                    }
 
-                                @Override
-                                public void onAnimationRepeat(Animation animation) {
-                                }
-                            });
-                            ivKettle.startAnimation(operatingAnim);
-                        }
+                                    @Override
+                                    public void onAnimationRepeat(Animation animation) {
+                                    }
+                                });
+                                ivKettle.startAnimation(operatingAnim);
+                            }
 
-                        @Override
-                        public void onAnimationRepeat(Animation animation) {
-                        }
-                    });
-                    ivKettle.startAnimation(operatingAnim);
+                            @Override
+                            public void onAnimationRepeat(Animation animation) {
+                            }
+                        });
+                        ivKettle.startAnimation(operatingAnim);
+                    } else {
+                        Animation operatingAnim = AnimationUtils.loadAnimation(WateringFruitTreesActivity.this, R.anim.rotate_anim);
+                        LinearInterpolator lin = new LinearInterpolator();
+                        operatingAnim.setInterpolator(lin);
+                        operatingAnim.setFillAfter(true);
+                        operatingAnim.setAnimationListener(new Animation.AnimationListener() {
+                            @Override
+                            public void onAnimationStart(Animation animation) {
+                                Animation animation1 = new AlphaAnimation(0f, 1.0f);
+                                animation1.setDuration(800);//动画的持续的时间
+                                animation1.setRepeatCount(1);
+                                ivFertilizer1.startAnimation(animation1);
+
+                                Animation animation2 = new AlphaAnimation(0f, 1.0f);
+                                animation2.setDuration(800);//动画的持续的时间
+                                animation2.setRepeatCount(1);
+                                ivFertilizer2.startAnimation(animation2);
+
+                                Animation animation3 = new AlphaAnimation(0f, 1.0f);
+                                animation3.setDuration(800);//动画的持续的时间
+                                animation3.setRepeatCount(1);
+                                ivFertilizer3.startAnimation(animation3);
+                            }
+
+                            @Override
+                            public void onAnimationEnd(Animation animation) {
+                                Animation operatingAnim = AnimationUtils.loadAnimation(WateringFruitTreesActivity.this, R.anim.rotate_anim_one);
+                                LinearInterpolator lin = new LinearInterpolator();
+                                operatingAnim.setInterpolator(lin);
+                                operatingAnim.setFillAfter(true);
+                                operatingAnim.setAnimationListener(new Animation.AnimationListener() {
+                                    @Override
+                                    public void onAnimationStart(Animation animation) {
+                                    }
+
+                                    @Override
+                                    public void onAnimationEnd(Animation animation) {
+                                        Animation animation1 = new AlphaAnimation(1.0f, 0f);
+                                        animation1.setDuration(1000);//动画的持续的时间
+                                        animation1.setFillAfter(true);
+                                        ivFertilizer.startAnimation(animation1);
+                                    }
+
+                                    @Override
+                                    public void onAnimationRepeat(Animation animation) {
+                                    }
+                                });
+                                ivFertilizer.startAnimation(operatingAnim);
+                            }
+
+                            @Override
+                            public void onAnimationRepeat(Animation animation) {
+                            }
+                        });
+                        ivFertilizer.startAnimation(operatingAnim);
+                    }
                 } else {
                     ToastUtils.showToast(statusModel.getMsg());
                 }
